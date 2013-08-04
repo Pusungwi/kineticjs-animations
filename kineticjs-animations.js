7c090f4a2932593638f1c9d7ed52dc75e7175dd7
Yi Yeon Jae <pusungwi@gmail.com>
2013-08-04 16:50:48 +0900

    working on TEST HTML rename animation js

--- kineticjs-animations.js
+++ kineticjs-animations.js
@@ -3,10 +3,9 @@
 // Copyright 2013 @ Yi 'Pusungwi' Yeon Jae
 // See LICENSE and README.md
 
-var FADE_TO_MAX_VALUE = 1.0;
-
 // fade in/out/to animations
 function fadeToAnimation(targetLayer, targetShape, period, dstOpacity, endFunc) {
+  var FADE_TO_MAX_VALUE = 1.0;
   var currentShapeOpacity = targetShape.getOpacity();
   var isFadeIn = null;
   if (currentShapeOpacity <= dstOpacity) {
@@ -42,6 +41,7 @@ function fadeToAnimation(targetLayer, targetShape, period, dstOpacity, endFunc) 
 }
 
 function fadeInAnimation(targetLayer, targetShape, period, endFunc) {
+  console.log("running fadeIn...")
   fadeToAnimation(targetLayer, targetShape, period, 1.0, endFunc);
 }
 
@@ -50,23 +50,25 @@ function fadeOutAnimation(targetLayer, targetShape, period, endFunc) {
 }
 
 //zoom in/out/to animations
-function zoomOutAnimation(targetLayer, targetShape, dstScale, endFunc) {
-  var currentShapeScale = targetShape.getScale();
-  var anim = new Kinetic.Animation(function(frame) {
-    var scale = currentShapeScale.x - (dstScale * (frame.time / period));
-    if (scale <= dstScale) {
-      scale = dstScale;
-      this.stop();
-      if (endFunc !== null) {
-        endFunc();
-      }
+function insertCenterScale(targetShape) {
+  if (targetShape.cx == null) {
+    //set new center scale method
+    targetShape.cx=targetShape.getX()+targetShape.getWidth()/2;
+    targetShape.cy=targetShape.getY()+targetShape.getHeight()/2;
+    // custom scale function to both
+    // scale the group and center the results
+    targetShape.setCenterScale=function(x,y){
+      targetShape.setScale(x,y);
+      targetShape.setPosition(
+        targetShape.cx-targetShape.getWidth()/2*targetShape.getScale().x,
+        targetShape.cy-targetShape.getHeight()/2*targetShape.getScale().y);
+      targetShape.draw();
     }
-    targetShape.setCenterScale(scale, scale);
-  }, rectLayer);
-  anim.start();
+  }
 }
 
-function zoomInAnimation(targetLayer, targetShape, dstScale, endFunc) {
+function zoomInAnimation(targetLayer, targetShape, dstScale, period, endFunc) {
+  insertCenterScale(targetShape);
   var currentShapeScale = targetShape.getScale();
   var anim = new Kinetic.Animation(function(frame) {
     var scale = currentShapeScale.x + (dstScale * (frame.time / period));
@@ -78,42 +80,72 @@ function zoomInAnimation(targetLayer, targetShape, dstScale, endFunc) {
       }
     }
     targetShape.setCenterScale(scale, scale);
-  }, rectLayer);
+  }, targetLayer);
   anim.start();
 }
 
-function zoomToAnimation(targetLayer, targetShape, dstScale, endFunc) {
+function zoomOutAnimation(targetLayer, targetShape, dstScale, period, endFunc) {
+  insertCenterScale(targetShape);
   var currentShapeScale = targetShape.getScale();
-  if (currentShapeScale >= dstScale) {
-    zoomInAnimation(targetLayer, targetShape, dstScale, endFunc);
+  var anim = new Kinetic.Animation(function(frame) {
+    var scale = currentShapeScale.x - (dstScale * (frame.time / period));
+    if (scale <= dstScale) {
+      scale = dstScale;
+      this.stop();
+      if (endFunc !== null) {
+        endFunc();
+      }
+    }
+    targetShape.setCenterScale(scale, scale);
+  }, targetLayer);
+  anim.start();
+}
+
+function zoomToAnimation(targetLayer, targetShape, dstScale, period, endFunc) {
+  var currentShapeScale = targetShape.getScale();
+  if (dstScale >= currentShapeScale.x) {
+    zoomInAnimation(targetLayer, targetShape, dstScale, period, endFunc);
   } else {
-    zoomOutAnimation(targetLayer, targetShape, dstScale, endFunc);
+    zoomOutAnimation(targetLayer, targetShape, dstScale, period, endFunc);
   }
 }
 
-//rotate
-function rotateToFunction(targetLayer, targetShape, dstDegree, endFunc) {
+// rotate
+function rotateToFunction(targetLayer, targetShape, dstDegree, period, endFunc) {
   // one revolution per 4 seconds
+  var angularSpeed = 1000;
   var anim = new Kinetic.Animation(function(frame) {
     var angularSpeed = Math.PI / 2;
-    var anim = new Kinetic.Animation(function(frame) {
     var angleDiff = frame.timeDiff * angularSpeed / 1000;
-    blueRect.rotate(angleDiff);
-    yellowRect.rotate(angleDiff);
-    redRect.rotate(angleDiff);
+    targetShape.rotate(angleDiff);
   }, layer);
 
   anim.start();
 }
 
-//move
+// move
 function moveToFunction(targetLayer, targetShape, targetX, targetY, period, endFunc) {
   var shapeX = targetShape.getX();
   var shapeY = targetShape.getY();
 
   var anim = new Kinetic.Animation(function(frame) {
-    targetShape.setX((targetX-shapeX) * (frame.time / period) + shapeX);
-    targetShape.setY((targetY-shapeY) * (frame.time / period) + shapeY);
+    var newX = (targetX-shapeX) * (frame.time / period) + shapeX;
+    var newY = (targetY-shapeY) * (frame.time / period) + shapeY;
+
+    
+    if (newX >= targetX) {
+      newX = targetX;
+    }
+    targetShape.setX(newX);
+    
+    if (newY >= targetY) {
+      newY = targetY;
+    }
+    targetShape.setY(newY);
+
+    if (newX >= targetX && newY >= targetY) {
+      this.stop();
+    }
   }, layer);
 
   anim.start();
